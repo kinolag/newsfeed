7e905bf2600b4bb033f0f83867a2521d14fc5759
Nicola <ngrassini@yahoo.it>
2020-08-14 17:49:03 +0200

    Updates

---
+++ precache-manifest.c0636a08f980d309c3653a7196c495be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33c31a1bc43b1cb208c3ab944e39f89",
+    "url": "/newsfeed/index.html"
+  },
+  {
+    "revision": "9bf3b9632aa3654fb548",
+    "url": "/newsfeed/static/css/main.552ae030.chunk.css"
+  },
+  {
+    "revision": "5b47a067a45e8e8997ca",
+    "url": "/newsfeed/static/js/2.99e03711.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newsfeed/static/js/2.99e03711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf3b9632aa3654fb548",
+    "url": "/newsfeed/static/js/main.d168547f.chunk.js"
+  },
+  {
+    "revision": "99215cd4c39e2c154cca",
+    "url": "/newsfeed/static/js/runtime-main.dcf4b789.js"
+  },
+  {
+    "revision": "258e2104a3e3bf6d588c05ed2d7b55b4",
+    "url": "/newsfeed/static/media/s_logo_1200.258e2104.png"
+  }
+]);
