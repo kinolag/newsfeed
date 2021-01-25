3a6da6fa61a3107bfcd62a29460fafc9474e87da
Nicola <ngrassini@yahoo.it>
2021-01-25 13:15:46 +0000

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,19 @@
 import React from "react";
-import slogo from "../assets/s_logo_1200.png";
 
 export default function Header() {
   return (
-    <div
-      className="center-wrapper bg-white"
-      style={{ paddingTop: "4px" }}
-    >
+    <div className="center-wrapper bg-white" style={{ paddingTop: "4px" }}>
       <div
         className="multi-wrapper radius-4 text-plain"
         style={{ border: "0px solid #d30d1d", width: "98%" }}
       >
-        <div style={{margin: "0px 8px"}}>
-          <h3 className="is-main-red news-headline">
-            Simple NewsFeed
-          </h3>
-        </div>
+        <h3 className="is-main-red news-headline" style={{ margin: "0px 8px" }}>
+          Simple NewsFeed
+        </h3>
         <div
-          className="row-wrapper is-almost-black"
-          style={{ alignItems: "center", margin: "0px 8px"}}
+          style={{ alignItems: "center", margin: "2px 8px", fontSize: "16px" }}
         >
-          <span className="is-grey" style={{ fontSize: "16px" }}>
-            A React app for
-          </span>
-          <div>
-            <img
-              style={{ height: "25px", width: "auto", padding: "0px 0px 4px 6px" }}
-              src={slogo}
-              alt="Logo"
-            />
-          </div>
-          
+          A React app using News API
         </div>
       </div>
     </div>
